b7cea32d1b5220a87f58c25b25bdb6b482d90f41
RonanDrouglazet <ronan.drouglazet@ebuzzing.com>
2016-02-02 21:58:49 +0100

    finish home

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 $(document).ready(function() {
 
 	// clone menu for fixed purpose
-	var menu = $('.menu')
-	var menu_fixed = menu.clone().addClass('fixed').css('display', 'none')
-	menu.parent().append(menu_fixed)
+	if (window.screen.width >= 740) {
+		var menu = $('.menu')
+		var menu_fixed = menu.clone().addClass('fixed').css('display', 'none')
+		menu.parent().append(menu_fixed)
 
-    $(window).scroll(function() {
-		if ($(window).scrollTop() >= menu.offset().top + menu.height() && menu_fixed.css('display') === 'none') {
-			menu_fixed.fadeIn(250)
-		} else if ($(window).scrollTop() < menu.offset().top && menu_fixed.css('display') !== 'none') {
-			menu_fixed.fadeOut(250)
-		}
-	})
+	    $(window).scroll(function() {
+			if ($(window).scrollTop() >= menu.offset().top + menu.height() && menu_fixed.css('display') === 'none') {
+				menu_fixed.fadeIn(250)
+			} else if ($(window).scrollTop() < menu.offset().top && menu_fixed.css('display') !== 'none') {
+				menu_fixed.fadeOut(250)
+			}
+		})
+	}
 
 })
 
