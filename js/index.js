c5994c575d94e63cb0a65b8150556151196c68d5
RonanDrouglazet <ronan.drouglazet@teads.tv>
2016-03-18 14:48:48 +0100

    mobile ready

--- js/index.js
+++ js/index.js
@@ -1,23 +1,34 @@
 $(document).ready(function() {
 
-	// clone menu for fixed purpose
-	if (window.screen.width >= 740) {
-		var menu = $('.menu')
-		var menu_fixed = $('.menu.fixed').css('display', 'none')
+    // clone menu for fixed purpose
+    if (window.screen.width >= 740) {
+        var menu = $('.menu')
+        var menu_fixed = $('.menu.fixed').css('display', 'none')
 
-	    $(window).scroll(function() {
-			if ($(window).scrollTop() >= menu.offset().top + menu.height() && menu_fixed.css('display') === 'none') {
-				menu_fixed.fadeIn(250)
-			} else if ($(window).scrollTop() < menu.offset().top && menu_fixed.css('display') !== 'none') {
-				menu_fixed.fadeOut(250)
-			}
-		})
-	}
+        $(window).scroll(function() {
+            if ($(window).scrollTop() >= menu.offset().top + menu.height() && menu_fixed.css('display') === 'none') {
+                menu_fixed.fadeIn(250)
+            } else if ($(window).scrollTop() < menu.offset().top && menu_fixed.css('display') !== 'none') {
+                menu_fixed.fadeOut(250)
+            }
+        })
+    }
 
-	$('article .button').click(function() {
-		$(this).parents('article').find('div.more').slideToggle()
-	})
-	$('div.more').slideUp(0)
+    $('article .button').click(function() {
+        $(this).parents('article').find('div.more').slideToggle()
+    })
+
+    $('div.more').slideUp(0)
+
+    if ($('.menu:not(.fixed)').css('display') === 'none') {
+        $('.menu.fixed').css('display', 'block')
+        $('.mobile_menu').fadeOut(0).click(function() {
+            $('.mobile_menu').fadeOut(0)
+        })
+        $('.mobile_menu.current').removeClass('current')
+    } else {
+        $('.menu.fixed .column:nth-child(1)').css('display', 'none')
+    }
 })
 
 
